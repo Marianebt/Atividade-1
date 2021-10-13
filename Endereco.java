e7f22d414c2fccc6ec5bad1f7c81c5be3484fef2
Marianebt <88720103+Marianebt@users.noreply.github.com>
2021-10-13 19:14:12 -0300

    Add files via upload
    Questão 2

---
+++ Endereco.java
@@ -0,0 +1,42 @@
+public class Endereco
+{
+	public static void main(String[] args) {
+	public String rua;
+	private	int numero;
+	private	String complemento;
+	public	String bairro;
+	public	String cidade;
+	public	String uf;
+	public	int cep;
+	    public void rua(){
+		   return rua = Joaquim Silva;
+		}
+		public void nomero(){
+		    return numero = 32;
+		}
+		public void complemento(){
+		    return complemento = Oliveira;
+		}
+		public void bairro(){
+		    return bairro = Caixa d' agua;
+		}
+		public void cidade(){
+		    return cidade = Euclides da cunha;
+		}
+		public void uf(){
+		    return uf = BA;
+		}
+		public void cep(){
+		    return cep = 48000;
+		}
+	    System.out.println("rua");
+	    System.out.println("numero");
+	    System.out.println("complemento");
+	    System.out.println("bairro");
+	    System.out.println("cidade");
+	    System.out.println("uf");
+	    System.out.println("cep");
+		
+		
+	}
+}
