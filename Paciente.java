55fbd3bae31710c96ac13a000b65bec15ecc573b
Marianebt <88720103+Marianebt@users.noreply.github.com>
2021-10-13 19:00:40 -0300

    Add files via upload
    Questão 1

---
+++ Paciente.java
@@ -0,0 +1,17 @@
+public class Paciente {
+    int codigo;
+    float dataNascimento;
+    String sexo;
+    String planoSaude;
+    String alergia;
+    String tipoSanguineo;
+    public Paciente (int codigo,float dataNascimento, String sexo, String planoSaude, String alergia, String tipoSanguineo) {
+        this.codigo = codigo;
+        this.dataNascimento = dataNascimento;
+        this.sexo = sexo;
+        this.planoSaude = planoSaude;
+        this.alergia = alergia;
+        this.tipoSanguineo = tipoSanguineo;
+        
+    }
+}
