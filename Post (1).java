2e6d8be5426dfda25f0a25aa31323bb57172bcd8
Marianebt <88720103+Marianebt@users.noreply.github.com>
2021-10-13 19:53:34 -0300

    Questão 4

---
+++ Post (1).java
@@ -0,0 +1,21 @@
+public class Post {
+   public String texto;
+   public String link;
+   private int numeroCurtidas;
+   private int numeroCompartilhamento;
+    public void texto (){
+        texto = (O universo sempre vai ajudar quem ousa sonhar);
+    }
+    public void link (){
+        link = (https://www.youtube.com/channel/UC0tkeCd64wiUr7ztjn5KaLQ);
+        
+    }
+    public void curtir (){
+        curtidas = numeroCurtidas * 3;
+        
+    }
+    curtir = numeroCurtidas + 1;
+    numeroCompartilhamento = numeroCompartilhamento + 1;
+    System.out.println("numeroCurtidas + curtidas + curtir");
+    System.out.println("numeroCompartilhamento");
+    
