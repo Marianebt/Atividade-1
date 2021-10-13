ed4b7dcba6ec4f0b71302312e476a6cdfb1bcd00
Marianebt <88720103+Marianebt@users.noreply.github.com>
2021-10-13 19:02:51 -0300

    Add files via upload
    Questão 2

---
+++ Medico.java
@@ -0,0 +1,33 @@
+public class Medico
+{
+	public static void main(String[] args) {
+	public int codigo;
+	private	String nome;
+	public	String sexo;
+	public	String especialidade;
+	public	String endereco;
+	    public void codigo(){
+		   return codigo = 32;
+		}
+		public void nome(){
+		    return nome = Pierrer;
+		}
+		public void especialidade(){
+		    return especialidade = clínico geral;
+		}
+		public void sexo(){
+		    return sexo = masculino;
+		}
+		public void especialidade(){
+		    return especialidade = clínico geral;
+		}
+		public void endereco(){
+		    return endereco = Euclides da cunha;
+		}
+		
+		System.out.println("codigo");
+		System.out.println("nome");
+		System.out.println("sexo");
+		System.out.println("especialidade");
+		System.out.println("endereco");
+		
