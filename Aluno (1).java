146624951268491b54e0b557bb4ee401f0533153
Marianebt <88720103+Marianebt@users.noreply.github.com>
2021-10-13 20:20:51 -0300

    Questão 5

---
+++ Aluno (1).java
@@ -0,0 +1,22 @@
+public class Aluno {
+    String nome;
+    int cpf;
+    float nota1;
+    float nota2;
+    float nota3;
+    nome = nome;
+    cpf = cpf;
+    getcalcularmedia = nota1 + nota2 + nota3 /3;
+        return calcularmedia;
+       cpf aluno = new aluno;
+       nome aluno = new aluno;
+       nota.nota1 = 10,0;
+       nota.nota2 = 10,0;
+       nota.nota3 = 9,5;
+     Mediatotal = calcularmedia;
+     System.out.println("calcularmedia");
+       
+        
+    }
+}
+    
